930a3d2c3c7305fcbd993615fd85351dab8aacad
bhargavvarmasagi <bhargavvarmasvs2001@gmail.com>
2024-04-04 22:34:29 -0500

    done

--- controllers/Balls.js
+++ controllers/Balls.js
@@ -66,3 +66,38 @@ res.send(`{"error": ${err}}`);
 }
 };
 
+//for a specific Costume.
+exports.balls_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await Balls.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
+
+
+//Handle Costume update form on PUT.
+exports.balls_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Balls.findById( req.params.id)
+// Do updates of properties
+if(req.body.ball_type)
+toUpdate.ball_type = req.body.ball_type;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+if(req.body.material) toUpdate.material = req.body.material;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
+
+
