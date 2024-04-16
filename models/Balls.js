69b9b048a4d17015c4742373c8d9abaeff510e2f
bhargavvarmasagi <bhargavvarmasvs2001@gmail.com>
2024-04-16 18:53:20 -0500

    done

--- models/Balls.js
+++ models/Balls.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose")
+
 const ballsSchema = mongoose.Schema({
-ball_type: String,
-cost: Number,
-material: String
+ball_type: {
+    type: String,
+    required: true,
+    minlength:3
+},
+cost: {
+    type:Number,
+    required: true,
+    min: 0,
+    max:100
+},
+material: {
+    type: String,
+    required: true,
+    minlength:4
+}
 })
+
 module.exports = mongoose.model("balls",
 ballsSchema)
